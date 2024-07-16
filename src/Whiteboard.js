458884590a16a4bd2a56a4ac6fba12aae2b292b1
PratyakshGupta48 <pratyakshg62@gmail.com>
2024-07-16 16:18:41 +0530

    First Commit

--- src/Whiteboard.js
+++ src/Whiteboard.js
@@ -2,7 +2,7 @@ import React, { useRef, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import io from 'socket.io-client';
 
-const socket = io('https://mysterious-river-36918-10bc48c38923.herokuapp.com');
+const socket = io('https://real-time-whiteboard-e7219d7af49a.herokuapp.com');
 
 const Whiteboard = () => {
   const { boardId } = useParams();
